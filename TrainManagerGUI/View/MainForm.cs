a2f38e0987e8f27999b693e8253a3acbb1664ab9
daniilvinn <86977774+daniilvinn@users.noreply.github.com>
2022-06-19 17:04:05 +0300

    Added GUI project and basic test

---
+++ TrainManagerGUI/View/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TrainManagerGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (View.TrainForm t = new View.TrainForm())
+            {
+                t.ShowDialog();
+            }
+        }
+    }
+}
