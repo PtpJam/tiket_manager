53e539f4439a07ba2a32d0f918294d97f19e51bf
daniilvinn <86977774+daniilvinn@users.noreply.github.com>
2022-06-19 15:57:10 +0300

    Hotfix: fixed project architecture

---
+++ DatabaseLibrary/Controller/Database/Seller_Controller.cs
@@ -0,0 +1,30 @@
+﻿using TicketManagerLibrary.Model;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace TicketManagerLibrary.Controller.Database
+{
+    public class Seller_Controller
+    {
+        public Seller_Controller()
+        {
+            var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
+
+            Sellers = connection.GetAll<Seller>() as List<Seller>;
+            connection.Close();
+        }
+        public void Add(Seller obj, SqlConnection connection)
+        {
+            Sellers.Add(obj);
+            connection.Insert<Seller>(obj);
+        }
+
+        public Seller this[int index]
+        {
+            get => Sellers[index];
+            private set => Sellers[index] = value;
+        }
+
+        public List<Seller> Sellers{ get; private set; }
+    }
+}
