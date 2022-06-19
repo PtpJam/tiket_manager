fd89effddecc9d799209406225ac40c422f8c7f1
Dima Ivanov <97175414+Xepobopa@users.noreply.github.com>
2022-06-19 14:51:05 +0200

    create database models

---
+++ Controller/Database/Ticket_Controller.cs
@@ -0,0 +1,30 @@
+﻿using TicketManagerLibrary.Model;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace TicketManagerLibrary.Controller.Database
+{
+    public class Ticket_Controller
+    {
+        public Ticket_Controller()
+        {
+            var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
+
+            Tickets = connection.GetAll<Ticket>() as List<Ticket>;
+            connection.Close();
+        }
+        public void Add(Ticket obj, SqlConnection connection)
+        {
+            Tickets.Add(obj);
+            connection.Insert<Ticket>(obj);
+        }
+
+        public Ticket this[int index]
+        {
+            get => Tickets[index];
+            private set => Tickets[index] = value;
+        }
+
+        public List<Ticket> Tickets{ get; private set; }
+    }
+}
---
+++ Controller/Database/Train_Controller.cs
@@ -0,0 +1,31 @@
+﻿using TicketManagerLibrary.Model;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace TicketManagerLibrary.Controller.Database
+{
+    public class Train_Controller
+    {
+        public Train_Controller()
+        {
+            var connection = new DatabaseConnection(new ControllerJson().jsonModel).connection;
+
+            Trains = connection.GetAll<Train>() as List<Train>;
+
+            connection.Close();
+        }
+        public void Add(Train obj, SqlConnection connection)
+        {
+            Trains.Add(obj);
+            connection.Insert<Train>(obj);
+        }
+
+        public Train this[int index]
+        {
+            get => Trains[index];
+            private set => Trains[index] = value;
+        }
+
+        public List<Train> Trains{ get; private set; }
+    }
+}
